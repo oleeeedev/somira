309189cab1c1803ad0b261dbf23ebc113d265893
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-03 19:51:55 +0100

    Add annotations for repository setup and entity management
    Introduce `@Collection`, `@DropEntitiesOnStart`, and `@DropIndexesOnStart` annotations for repository handling. Add `@Id` annotation to mark entity identifier fields, aiding in entity management and configuration. These changes improve flexibility and clarity in repository setup.

---
+++ src/main/java/dev/ole/somira/repository/Collection.java
@@ -0,0 +1,13 @@
+package dev.ole.somira.repository;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Collection {
+
+    String value();
+}
---
+++ src/main/java/dev/ole/somira/repository/entity/Id.java
@@ -0,0 +1,9 @@
+package dev.ole.somira.repository.entity;
+
+import java.lang.annotation.*;
+
+@Inherited
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Id {
+}
