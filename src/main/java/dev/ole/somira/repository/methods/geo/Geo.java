5c7e6965eb57a77811687f12fcd3d57f92497476
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-03 20:25:50 +0100

    Add Geo and GeoType classes for geospatial operations
    Introduced the `Geo` class to represent geospatial data with methods for setting coordinates, distances, and query types. Added the `GeoType` enum to define supported query types, such as NEAR and WITHIN. This provides a foundational structure for geospatial query handling.

---
+++ src/main/java/dev/ole/somira/repository/methods/geo/Geo.java
@@ -0,0 +1,62 @@
+package dev.ole.somira.repository.methods.geo;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+public class Geo {
+
+    public static Geo of() {
+        return new Geo();
+    }
+
+    double latitude;
+    double longitude;
+    Double maxDistance;
+    Double minDistance;
+    GeoType type;
+
+    private Geo() {
+        type = GeoType.NEAR_SPHERE;
+    }
+
+    public Geo latitude(double latitude) {
+        this.latitude = latitude;
+        return this;
+    }
+
+    public Geo longitude(double longitude) {
+        this.longitude = longitude;
+        return this;
+    }
+
+    public Geo coordinates(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        return this;
+    }
+
+    public Geo maxDistance(double maxDistance) {
+        this.maxDistance = maxDistance;
+        return this;
+    }
+
+    public Geo minDistance(double minDistance) {
+        this.minDistance = minDistance;
+        return this;
+    }
+
+    public Geo near() {
+        this.type = GeoType.NEAR;
+        return this;
+    }
+
+    public Geo nearSphere() {
+        this.type = GeoType.NEAR_SPHERE;
+        return this;
+    }
+}
---
+++ src/main/java/dev/ole/somira/repository/methods/geo/GeoType.java
@@ -0,0 +1,9 @@
+package dev.ole.somira.repository.methods.geo;
+
+public enum GeoType {
+
+    NEAR,
+    NEAR_SPHERE,
+    INTERSECT,
+    WITHIN
+}
