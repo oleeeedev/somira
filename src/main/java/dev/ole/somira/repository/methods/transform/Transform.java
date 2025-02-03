0cc244a676a99edb6138bec4c93ec8d7ee858db5
oleeeedev <oleschumacherr1801@gmail.com>
2025-02-03 20:25:16 +0100

    Add Transform and NestedField annotations
    Introduced `@Transform` and `@NestedField` annotations to support runtime metadata for method-level transformations. These annotations enable defining transformation logic, nested query keys, and their grouping through `NestedKeyArray`.

---
+++ src/main/java/dev/ole/somira/repository/methods/transform/Transform.java
@@ -0,0 +1,13 @@
+package dev.ole.somira.repository.methods.transform;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Transform {
+
+    String value();
+}
